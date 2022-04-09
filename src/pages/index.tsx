e21b4818c2f5a5e1832954660fc6d2ffedb17e20
Washington Junior <washingtonjrt@gmail.com>
2022-04-09 15:51:09 -0300

    styling application

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,57 @@
+import { useState } from "react";
+
 export default function Home() {
-  return <h1>Operation Namemaker</h1>;
+  const [operationName, setOperationName] = useState("Tap to generate");
+  const [initialGeneration, setInitialGeneration] = useState(true);
+
+  function generateOperationName() {
+    const color = "Blue";
+    const animal = "Horse";
+
+    const newOperationName = `${color} ${animal}`;
+    setOperationName(newOperationName);
+    setInitialGeneration(false);
+  }
+
+  return (
+    <main className="p-8 bg-slate-100 w-screen h-screen flex flex-col flex-wrap place-content-center">
+      <h1 className="pb-4 text-center font-bold text-3xl text-gray-800">
+        Operation Namemaker
+      </h1>
+      <h2 className="pb-8 text-center text-gray-400">
+        Tap the{" "}
+        <span className="text-indigo-700 font-medium">refresh button</span> to
+        generate a new operation name
+      </h2>
+      <div className="p-6 pl-8 gap-6 w-full max-w-xl bg-gray-50 rounded-lg flex align-center justify-between">
+        <p
+          className={`grow m-auto  text-xl ${
+            initialGeneration
+              ? "italic text-gray-400"
+              : "font-semibold text-indigo-700"
+          }`}
+        >
+          {operationName}
+        </p>
+        <button
+          onClick={generateOperationName}
+          className="rounded-full transition duration-150 hover:bg-indigo-50 hover:scale-105 active:scale-90 active:bg-indigo-200"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-10 w-10 p-2 stroke-gray-400 transition duration-150 hover:stroke-indigo-700"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+            />
+          </svg>
+        </button>
+      </div>
+    </main>
+  );
 }
